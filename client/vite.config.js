557fb78d674921b97f94a590af6a84cb3247b3d6
sabarim6369 <sabarim6369@gmail.com>
2025-09-05 22:41:58 +0530

    initialized tailwindcss

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
     VitePWA({
       registerType: 'autoUpdate',  // keeps service worker updated
       includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
