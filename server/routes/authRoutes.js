304442ffbb226b1a7f6922b9d43a7a2f0db0b49f
sabarim6369 <sabarim6369@gmail.com>
2025-09-06 09:18:48 +0530

    Working on profile pic adding and updation

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,9 +1,13 @@
 const express = require('express');
-const { register, login } = require('../controllers/authController');
+const { register, login, getProfile, updateProfile } = require('../controllers/authController');
+const { authMiddleware } = require('../Middleware/authmiddleware');
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
+router.get('/profile', authMiddleware, getProfile);
+router.put('/profile', authMiddleware, updateProfile);
+
 
 module.exports = router;
