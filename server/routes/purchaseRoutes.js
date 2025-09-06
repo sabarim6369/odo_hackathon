cbf9774c728268eda307a9ae01b55c797691c33a
sabarim6369 <sabarim6369@gmail.com>
2025-09-06 12:15:07 +0530

    Working on cancel purchase option

--- server/routes/purchaseRoutes.js
+++ server/routes/purchaseRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { checkout, getPurchases} = require('../controllers/purchaseController');
+const { checkout, getPurchases,cancelPurchase} = require('../controllers/purchaseController');
 const { authMiddleware } = require('../Middleware/authmiddleware');
 
 const router = express.Router();
 
 router.post('/checkout', authMiddleware, checkout);
 router.get('/', authMiddleware, getPurchases);
+router.post('/cancel', authMiddleware, cancelPurchase);
+
 
 module.exports = router;
